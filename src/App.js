aba823ac267e95c836c884ee205aaff1b32eca0e
vistritpandey <vistritpandey@gmail.com>
2020-12-16 16:37:38 +0530

    tindercards added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      
+      <TinderCards />
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TinderCards.css";
+
+function TinderCards() {
+  return (
+    <div className="tinderCards">
+      <p>Cards</p>
+    </div>
+  );
+}
+
+export default TinderCards;
